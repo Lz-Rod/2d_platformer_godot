caacd37f090d1adecc24f0d3072fbc8afa7b9466
Lz-Rod <lz.rod@outlook.com>
2025-11-27 18:44:46 -0300

    implemented player state machine

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,38 +1,92 @@
 extends CharacterBody2D
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
+enum PlayerState {
+	idle,
+	walk,
+	jump
+}
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 80.0
 const JUMP_VELOCITY = -300.0
 
+var status: PlayerState
+
+#essa função inicia o jogo em idle
+func _ready() -> void:
+	go_to_idle_state()
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	#adiciona a gravidade
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	match status:
+		PlayerState.idle:
+			idle_state()
+		PlayerState.walk:
+			walk_state()
+		PlayerState.jump:
+			jump_state()
+			
+	move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+#essas funções fazem o estado alterar
+func go_to_idle_state():
+	status = PlayerState.idle
+	anim.play("idle")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func go_to_walk_state():
+	status = PlayerState.walk
+	anim.play("walk")
+	
+func go_to_jump_state():
+	status = PlayerState.jump
+	anim.play("jump")
+	velocity.y = JUMP_VELOCITY
+
+#essas funções determinam o que acontece em cada estado
+func idle_state():
+	move()
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+		
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+	
+func walk_state():
+	move()
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+	
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+	
+func jump_state():
+	move()
+	if is_on_floor():
+		
+		if velocity.x == 0:
+			go_to_idle_state()			
+		else:
+			go_to_walk_state()
+		return
+
+#atualiza a velocidade do player e faz a animação flipar
+func move():
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if is_on_floor():
-		#muda a direção do sprite e animações entre idle e walk
-		if direction > 0:
-			anim.flip_h = false
-			anim.play("walk")
-		elif direction < 0:
-			anim.flip_h = true
-			anim.play("walk")
-		else:
-			anim.play("idle")
-	else:
-		anim.play("jump")
 
-	move_and_slide()
+	if direction < 0:
+		anim.flip_h = true		
+	elif direction > 0:
+		anim.flip_h = false
