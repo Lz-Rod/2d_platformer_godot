bc5e1685dadaa052643bd11f6e0e63fca391ced6
Lz-Rod <lz.rod@outlook.com>
2026-01-29 18:17:57 -0300

    Fixed player death, and developed stage reload.

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,7 @@ enum PlayerState {
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var coll: CollisionShape2D = $CollisionShape2D
+@onready var reload: Timer = $"Timer-reload"
 
 @export var max_speed = 180.0
 @export var acceleration = 400
@@ -94,6 +95,7 @@ func go_to_hurt_state():
 	status = PlayerState.hurt
 	anim.play("hurt")
 	velocity = Vector2.ZERO
+	reload.start()
 
 #essas funções determinam o que acontece em cada estado
 func idle_state(delta):
@@ -212,4 +214,9 @@ func _on_area_2_dhitbox_area_entered(area: Area2D) -> void:
 		go_to_jump_state()
 	else:
 		#player morre
-		go_to_hurt_state()
+		if status != PlayerState.hurt:
+			go_to_hurt_state()
+
+
+func _on_timerreload_timeout() -> void:
+	get_tree().reload_current_scene()
